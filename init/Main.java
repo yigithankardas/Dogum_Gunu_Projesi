140e8f0419dafadac19a0b08d7843ffea4899800
Yiğithan Kardaş <78052319+yigithankardas@users.noreply.github.com>
2022-01-23 17:09:00 +0300

    Update Main.java

--- init/Main.java
+++ init/Main.java
@@ -7,7 +7,7 @@ import gui.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		File file = new File(System.getenv("USERPROFILE") + "/OneDrive - TOBB Ekonomi ve Teknoloji Üniversitesi/DogumGunleri.txt");
+		File file = new File(System.getenv("USERPROFILE") + "/DogumGunleri.txt");
 		boolean exist = false;
 		try {
 			exist = file.createNewFile();
@@ -18,7 +18,7 @@ public class Main {
 		GUI frame = new GUI(handler);
 		frame.initiate();
 		
-		if (exist == true) {	// dosya oluþturuldu
+		if (exist == true) {	// dosya oluÅŸturuldu
 			GUI.firstTime();
 		}
 	}
